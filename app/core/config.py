a8203a6dbb72f298663dc97438a271478e4fac77
Danylo-D87 <quaestor88@gmail.com>
2025-06-16 16:41:05 +0300

    replace config

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,12 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine
+
+
 SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./library.db"
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+Base = declarative_base()
